8905cd7d6cb0219c7e3d7216f450696259f3a177
sohaibzia23 <sohaibzia89@gmail.com>
2024-11-23 13:59:14 -0700

    first commit

---
+++ src/main/java/com/example/EcoSight/EcoSightApplication.java
@@ -0,0 +1,13 @@
+package com.example.EcoSight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoSightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoSightApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/EcoSight/EcoSightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EcoSight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoSightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
