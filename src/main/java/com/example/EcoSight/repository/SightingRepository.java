b66e809f44c74b0cea46f4c167b98d7b7869678a
Eyram Ekpe <51966182+BankSinatra@users.noreply.github.com>
2024-12-02 19:51:50 -0700

    Deleted SightingRepository

--- src/main/java/com/example/EcoSight/repository/SightingRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.EcoSight.repository;
-
-import com.example.EcoSight.entity.Sighting.Sighting;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SightingRepository extends JpaRepository<Sighting, Integer> {
-}
