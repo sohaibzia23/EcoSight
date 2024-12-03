9c126e0dd0138034e16ae72d65036857f6ad4fca
Eyram Ekpe <51966182+BankSinatra@users.noreply.github.com>
2024-12-02 18:39:00 -0700

    Deleted a bunch for stability reasons

--- src/main/java/com/example/EcoSight/repository/AdministratorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.EcoSight.repository;
-
-import com.example.EcoSight.entity.Administrator;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AdministratorRepository extends JpaRepository<Administrator, Integer> {
-}
