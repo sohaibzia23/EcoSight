56044fc0d17d4e216fb4c8dcf2e7796e3d7b4bf0
Eyram Ekpe <51966182+BankSinatra@users.noreply.github.com>
2024-12-03 23:33:46 -0700

    getSightingsByUserId api call works now

--- src/main/java/com/example/EcoSight/entity/Sighting/Sighting.java
+++ src/main/java/com/example/EcoSight/entity/Sighting/Sighting.java
@@ -4,6 +4,7 @@ import com.example.EcoSight.entity.Contributor;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
@@ -11,6 +12,7 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name="sighting")
 public class Sighting {
