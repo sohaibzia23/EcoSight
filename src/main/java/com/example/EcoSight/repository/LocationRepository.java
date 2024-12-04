4d93bda11700473c994c26305a7c4fa8917df578
Eyram Ekpe <51966182+BankSinatra@users.noreply.github.com>
2024-12-04 14:36:37 -0700

    Changed LocationRepository a little

--- src/main/java/com/example/EcoSight/repository/LocationRepository.java
+++ src/main/java/com/example/EcoSight/repository/LocationRepository.java
@@ -3,6 +3,12 @@ package com.example.EcoSight.repository;
 import com.example.EcoSight.entity.Location.LatLong;
 import com.example.EcoSight.entity.Location.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface LocationRepository extends JpaRepository<Location, LatLong> {
+    @Query("SELECT l FROM Location l WHERE l.sighting.sightingId IN :sightingIds")
+    List<Location> findLocationsBySightingIds(@Param("sightingIds") List<Integer> sightingIds);
 }
