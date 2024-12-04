cf2e96d0094c2d4685755d927ccaada6cc28aa47
Eyram Ekpe <51966182+BankSinatra@users.noreply.github.com>
2024-12-03 17:43:46 -0700

    Removed unnecessary stuff to the SightingDTO

--- src/main/java/com/example/EcoSight/dto/SightingDTO.java
+++ src/main/java/com/example/EcoSight/dto/SightingDTO.java
@@ -1,7 +1,6 @@
 package com.example.EcoSight.dto;
 
 import com.example.EcoSight.entity.Sighting.SightingStatus;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,13 +13,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class SightingDTO {
     private Integer sightingId;
-
-    @NotNull(message = "Time cannot be null")
     private LocalDateTime time;
-
-    @NotNull(message = "Contributor ID cannot be null")
     private Integer contributorId;
-
-    @NotNull(message = "Status cannot be null")
     private SightingStatus status;
 }
