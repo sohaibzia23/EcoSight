1d85cc2264b4d8d0db5e5d03ef97508f063eede4
Eyram Ekpe <51966182+BankSinatra@users.noreply.github.com>
2024-12-04 16:27:55 -0700

    Added getLocationBySightingIds function in LocationService

--- src/main/java/com/example/EcoSight/services/LocationService.java
+++ src/main/java/com/example/EcoSight/services/LocationService.java
@@ -1,17 +1,29 @@
 package com.example.EcoSight.services;
 
 
+import com.example.EcoSight.dto.LocationDto;
 import com.example.EcoSight.entity.Location.LatLong;
 import com.example.EcoSight.entity.Location.Location;
+import com.example.EcoSight.mapping.LocationMapper;
 import com.example.EcoSight.repository.LocationRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Component
 @RequiredArgsConstructor
 public class LocationService {
-
     private final LocationRepository locationRepository;
+    private final LocationMapper locationMapper;
+
+    public List<LocationDto> getLocationsBySightingIds(List<Integer> sightingIds) {
+        List<Location> locations = locationRepository.findLocationsBySightingIds(sightingIds);
+        return locations.stream()
+                .map(locationMapper::toDTO)
+                .collect(Collectors.toList());
+    }
 
     public Location addLocation(Location location) {
         return locationRepository.save(location);
