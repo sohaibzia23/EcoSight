627e359d99c5684f3f6d4bae436882949697a5dc
Eyram Ekpe <51966182+BankSinatra@users.noreply.github.com>
2024-12-05 19:02:07 -0700

    Removed unnecessary imports

--- src/main/java/com/example/EcoSight/entity/User/User.java
+++ src/main/java/com/example/EcoSight/entity/User/User.java
@@ -2,9 +2,6 @@ package com.example.EcoSight.entity.User;
 
 import jakarta.persistence.*;
 import lombok.*;
-import org.hibernate.annotations.CreationTimestamp;
-
-import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
