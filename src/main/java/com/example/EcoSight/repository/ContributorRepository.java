bd06dab808108a6300a6f5407db674f034ffe4e0
Eyram Ekpe <51966182+BankSinatra@users.noreply.github.com>
2024-12-06 19:15:54 -0700

    Clean up

--- src/main/java/com/example/EcoSight/repository/ContributorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.EcoSight.repository;
-
-import com.example.EcoSight.entity.User.Contributor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ContributorRepository extends JpaRepository<Contributor, Integer> {
-}
